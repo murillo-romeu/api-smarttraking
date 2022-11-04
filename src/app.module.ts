84166db9858efce971a29e6b27dc49516fac320f
Murillo Romeu <murillo.romeu@roit.com.br>
2022-11-04 08:50:48 -0300

    Iniciando o schema mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://murillosi:crumu147@cluster0.psh2vx5.mongodb.net/smartraking?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true, 
+      useCreateIndex: true, 
+      useUnifiedTopology: true, 
+      useFindAndModify: false
+    }),
+    JogadoresModule
+  ],
   controllers: [],
   providers: [],
 })
